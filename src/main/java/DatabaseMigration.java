74407256428b485a2324fc93020eade143af50f4
dmytropoliashenko <polyashenko1@gmail.com>
2023-09-20 18:17:03 +0300

    move out main

--- src/main/java/DatabaseMigration.java
+++ src/main/java/DatabaseMigration.java
@@ -31,8 +31,7 @@ public class DatabaseMigration {
             " HAVING" +
             " COUNT(a.id) < 2";
 
-    public static void main(String[] args) {
-
+    public static void run () {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         } catch (Exception ex) {
